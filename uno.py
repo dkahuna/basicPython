5b79f56d28100b41d7287a8e27a8e6cbb6c34095
Khamhuang, David <khameye19888@gmail.com>
2020-01-25 16:45:28 -0600

    more methods shown

--- uno.py
+++ uno.py
@@ -35,8 +35,11 @@ print("You are " + age + " " + "year(s) old") # storing the user's input will gi
 
 # Lines 28 & 30 are examples of concatenating values, strings, etc.
 
-print("-----Lists-----")
+print("-----Lists-----") #collections of data types
 
 food = ["Wings", "What-A-Burger", "Donatos", "Surf&Turf", "SoCal Taco"] #storing more than one
 print(food)
 print(food[1]) # counting of a list starts at zero not 1
+
+spanishLine = "Buenos dias, amigo."
+print(spanishLine.split(' ')) #this will break the string into a list due to space argument passed in
