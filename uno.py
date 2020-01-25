7d1003a84de3f8400724d046fd138ddb25e8daea
Khamhuang, David <khameye19888@gmail.com>
2020-01-25 15:20:42 -0600

    nums and strings

---
+++ uno.py
@@ -0,0 +1,12 @@
+# everything in Python is considered 'OBJECTS' and note that objects have "attributes" & "methods"
+
+# and "methods" are respectedly functions towards the 'object' at hand
+
+# understanding of numbers, strings, and lists
+
+# print() <--- is key!!!! Unless within cmd
+
+print(type(5))      # returns the <class 'int'> for integer
+
+print(type(10.5))  # returns the <class 'float'> for non-whole numbers
+
