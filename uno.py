e18f4aee0cfafe91a0d82561ecd940dd88f20449
Khamhuang, David <khameye19888@gmail.com>
2020-01-25 16:03:26 -0600

    strings and concatenating

--- uno.py
+++ uno.py
@@ -6,12 +6,23 @@
 
 # print() <--- is key!!!! Unless within cmd
 
+print("----------------------------")
 print(type(5))      # returns the <class 'int'> for integer
-
+print("----------------------------")
 print(type(10.5))  # returns the <class 'float'> for non-whole numbers
-
+print("----------------------------")
 print(5 + 5)  #returns 10
 print(5 - 5) #returns 0
 print(5 * 5) #returns 25
 print(5 / 5) #returns 1.0 <-- which is a float
 print(5 // 5) #returns 1 <-- which is an integer
+print(5 ** 5) #returns the power of; 3125
+print(25 % 2) #returns the remainder 1
+
+print("-----Strings-----")
+
+name = "Kahuna" #this is storing value
+print(name) #returns 'Kahuna'
+
+age = input("How old are you? ") # this function prompts the user
+print("You are " + age + "year(s) old") # storing the user's input will give feedback
