cb9a6ff802021c6aefd2b6c9d753ccbcf8eb2674
Khamhuang, David <khameye19888@gmail.com>
2020-01-26 17:24:33 -0600

    reset

--- Lessons/uno.py
+++ Lessons/uno.py
@@ -23,3 +23,12 @@ num = 88    #storing an integer
 num += 2   #this changes the value due to having the equal sign after the operation
 print(num)  #returns 90
 
+
+
+
+
+# Calculating the area of a circle
+radius = input('Enter the radius of your circle (m): ')
+area = 3.142 * int(radius)**2  # int() function is used to convert the user's input from string to integer
+print('The area of your circle is:', area) 
+
---
+++ Projects/calcProgram.py
@@ -0,0 +1,4 @@
+# Calculating the area of a circle
+radius = input('Enter the radius of your circle (m): ')
+area = 3.142 * int(radius)**2  # int() function is used to convert the user's input from string to integer
+print('The area of your circle is:', area) 
