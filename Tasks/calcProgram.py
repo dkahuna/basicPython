e8877d8f81e7ea7adc160758847b621c470ade10
Khamhuang, David <khameye19888@gmail.com>
2020-01-26 17:21:21 -0600

    removed and renamed

--- Tasks/calcProgram.py
+++
@@ -1,4 +0,0 @@
-# Calculating the area of a circle
-radius = input('Enter the radius of your circle (m): ')
-area = 3.142 * int(radius)**2  # int() function is used to convert the user's input from string to integer
-print('The area of your circle is:', area) 
