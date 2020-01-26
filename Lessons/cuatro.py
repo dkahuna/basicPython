7f8a5f4689d34fe5f6a0e3f882737cc21d28512b
Khamhuang, David <khameye19888@gmail.com>
2020-01-26 17:58:26 -0600

    running elif

---
+++ Lessons/cuatro.py
@@ -0,0 +1,7 @@
+# if-statements which are for meeting certain conditions and outputting their values
+
+age = int(input('Enter your age: '))
+
+if age < 21:
+    # code block / notice that 'indenting' is very IMPORTANT in Python
+    print('You are not of legal age to purchase alcohol.')
