026b3ac0396471fa1a7be3d3db99076d8dddbb18
Khamhuang, David <khameye19888@gmail.com>
2020-01-26 19:52:29 -0600

    next is while loops

--- Lessons/forLoo.py
+++ Lessons/forLoo.py
@@ -8,6 +8,8 @@ cars = ['accord', 'celica', 240, 'hardbody', 'camaro', 'acadia', 'gsxr', 'G35', 
 # for car in cars[1:3]:       # <-- "slicing" was used to gives us part of the list
     # print(car)
 
+
+# as this function goes thru the collection( 'list' ) you can see that the condition statement written will cycle thru and output the values to what is met
 for car in cars:
     if car == 'gsxr':
             print(f'{car} was my first motorcycle')
