6a6ada985d5385a27ee5c5f2a4c98a4e230a552f
Khamhuang, David <khameye19888@gmail.com>
2020-01-26 19:44:18 -0600

    for loops and elif

--- Lessons/forLoo.py
+++ Lessons/forLoo.py
@@ -1,3 +1,15 @@
 # For-Loops
 
 cars = ['accord', 'celica', 240, 'hardbody', 'camaro', 'acadia', 'gsxr', 'G35', 'tundra', 'optima']
+
+# for car in cars:
+#     print(car) 
+
+# for car in cars[1:3]:       # <-- "slicing" was used to gives us part of the list
+    # print(car)
+
+for car in cars:
+    if car == 'gsxr':
+            print(f'{car} was my first motorcycle')
+    else:
+            print(f'{car} was one of the many vehicles I\'ve owned.')
