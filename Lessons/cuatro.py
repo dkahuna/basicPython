09fbf42018600d195501146d64b770ab5a472549
Khamhuang, David <khameye19888@gmail.com>
2020-01-26 18:07:38 -0600

    if,elif,else

--- Lessons/cuatro.py
+++ Lessons/cuatro.py
@@ -5,3 +5,5 @@ age = int(input('Enter your age: '))
 if age < 21:
     # code block / notice that 'indenting' is very IMPORTANT in Python
     print('You are not of legal age to purchase alcohol.')
+elif age >= 21:
+    print('You are able to purchase alcohol at this establishment.')
