b87d145d37088fa0dcf613e9b3f8d9d69199f4f3
Khamhuang, David <khameye19888@gmail.com>
2020-01-29 14:36:27 -0600

    CONTINUE understanding

--- Lessons/while.py
+++ Lessons/while.py
@@ -6,8 +6,11 @@ num = 0
 
 while num < age:
     # block of code       *alert* switching the lines of code 9&10 will output different results; one will spit out the num zero first and the other the number one
+    if num == 0:
+        num += 1
+        continue
     if num % 2 == 0:            #practicing a conditional statement within the loop
         print(num)
-    if num > 10:
+    if num >= 9:
         break          #this here is to stop the 'while loop' from running after a condition is met
     num += 1
