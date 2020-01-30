90adf78dc04ba1c38c658b443453e42bbbb4db7e
Khamhuang, David <khameye19888@gmail.com>
2020-01-30 14:51:40 -0600

    ranges feat

---
+++ Lessons/rangeR.py
@@ -0,0 +1,16 @@
+# RANGES
+
+    # a method to cycle/run thru a list/array of numbers that is passed through in the argument(s) but note that it not inclusive
+
+
+# this will print everthing from 0 to 7, not 8
+for spot in range(8):
+    print(spot)
+
+print("______")
+
+# passing more than one in the argument will give it a starting point but same thing occurs with not printing the end point
+for place in range(5, 13):
+    print(place)
+
+# a third argument can be passed thru for 
