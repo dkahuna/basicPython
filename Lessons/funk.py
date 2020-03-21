edaa0b8f9d98ce55073e38f97162c028af52f880
Khamhuang, David <khameye19888@gmail.com>
2020-03-21 15:17:38 -0500

    def(s) = functions

--- Lessons/funk.py
+++ Lessons/funk.py
@@ -1,5 +1,8 @@
-def greet():
-    print("Hello World!")
+def greet(name, time):
+    print(f'Good {time} {name}, hope you are well!' )
 
+# know that the variables could be named anything and it would still work
+name = input("May I have your name: ")
+time = input("What time of the day is it where you're at: ")
 
-greet()
+greet(name, time)
