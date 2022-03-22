9d83c9aec81d93b9b43bb07fce1fd6d3eeffa64f
Khamhuang, David <khameye19888@gmail.com>
2022-03-22 00:47:14 -0500

    more dictionaries with tuples

--- FCC/ex_09/ex_09.py
+++ FCC/ex_09/ex_09.py
@@ -2,6 +2,21 @@ fname = input('Enter file name: ')
 if len(fname) < 1 : fname = 'clown.txt'
 hand = open(fname)
 
+di = dict() #making an empty dictionary
 for line in hand :
-    line = line.rstrip()
-    print(line)
+    line = line.rstrip() #this line strips the white space on the right side per line of the file
+    # print(line)
+    words = line.split() #this line splits the text file into an array
+    # print(words)
+    for wds in words:
+        print(wds)
+        if wds in di :
+            di[wds] = di[wds] +1
+            print('**Existing**')
+        else:
+            di[wds] = 1
+            print('**NEW**')
+        print(di[wds])
+
+print(di)
+
