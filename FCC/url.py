422b4e9cae715b928859f0ee820f72043baafc2c
Khamhuang, David <khameye19888@gmail.com>
2022-03-28 13:31:25 -0500

    comments added for url library

--- FCC/url.py
+++ FCC/url.py
@@ -1,5 +1,7 @@
 import urllib.request, urllib.parse, urllib.error
 
+# library that makes the socket call to 'GET' the url data
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 for line in fhand:
+    # decrypts the lines from URL to UTF-8 array and then strips it into a string
     print(line.decode().strip())
