bd7970b8be9902ae4c7b2666a893ee538bc0b886
Khamhuang, David <khameye19888@gmail.com>
2022-08-03 13:15:40 -0500

    updating practice with functions

--- Lessons/funk.py
+++ Lessons/funk.py
@@ -1,15 +1,25 @@
-def greet(name, time):
-    print(f'Good {time} {name}, hope you are well!' )
+# def greet(name, time):
+#     print(f'Good {time} {name}, hope you are well!' )
 
 # know that the variables could be named anything and it would still work
-name = input("May I have your name: ")
-time = input("What time of the day is it where you're at: ")
+# name = input("May I have your name: ")
+# time = input("What time of the day is it where you're at: ")
 
-greet(name, time)
+# greet(name, time)
 
 
 # to put a default value to the parameters of a function if function is called without any arguments (e.g. line 15) is with the '=' operator 
-def greet(name = 'ryu', time = 'morning'):
-    print(f'Good {time} {name}, hope you are well!')
+# def greet(name = 'ryu', time = 'morning'):
+#     print(f'Good {time} {name}, hope you are well!')
 
-greet()
+# greet()
+
+print("========Page 46 Python book==========")
+
+def print_seconds_per_day(days):
+    hours = days * 24
+    minutes = hours * 60
+    seconds = minutes * 60
+    print(seconds)
+
+print_seconds_per_day(7)
