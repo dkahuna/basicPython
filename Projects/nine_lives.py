321bfe62981a70843d5b9f75f1e4955191f1b01a
Khamhuang, David <khameye19888@gmail.com>
2022-08-11 18:55:10 -0500

    winning feature

--- Projects/nine_lives.py
+++ Projects/nine_lives.py
@@ -3,26 +3,38 @@ import random
 
 lives = 9
 
-words = ['pizza', 'fairy', 'teeth', 'shirt', 'otter', 'plane']
+words = ['pizzas', 'fairy', 'teeth', 'tshirt', 'otters', 'planes']
 
 secret_word = random.choice(words)
 
-clue = list('?????')
+clue = []
+
+index = 0
+
+while index < len(secret_word):
+    clue.append('?')
+    index = index + 1
 
 heart_symbol = u'\u2764'
 
 guessed_word_correctly = False
 
+# announcing the user winning when last letter is guessed correctly
+unknown_letters = len(secret_word)
+
 
 # define/declare a function before the main program
-def update_clue(guessed_letter, secret_word, clue):
+def update_clue(guessed_letter, secret_word, clue, unknown_letters):
 
     index = 0
 
     while index < len(secret_word):
         if guessed_letter == secret_word[index]:
             clue[index] = guessed_letter
+            unknown_letters = unknown_letters - 1
         index = index + 1
+
+    return unknown_letters
 
 
 #MAIN
@@ -30,11 +42,11 @@ difficulty = input('Choose difficulty (type 1, 2, or 3):\n 1 Easy\n 2 Normal\n 3
 difficulty = int(difficulty)
 
 if difficulty == 1:
-    lives = 7
+    lives = 12
 elif difficulty == 2:
-    lives = 5
+    lives = 9
 else:
-    lives = 3
+    lives = 5
 
 
 while lives > 0:
@@ -47,11 +59,14 @@ while lives > 0:
         break
 
     if guess in secret_word:
-        update_clue(guess, secret_word, clue)
-
+        unknown_letters = update_clue(guess, secret_word, clue, unknown_letters)
     else:
         print('Incorrect. You lose a life!')
         lives = lives - 1
+
+    if unknown_letters == 0:
+        guessed_word_correctly = True
+        break
 
 if guessed_word_correctly:
     print('You won! The secret word is ' + secret_word)
